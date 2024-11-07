2e8b1aaa970b3122fbf770f84d168b5c94bd9041
AlexSzeto <Alex.Szeto@gmail.com>
2024-11-06 18:25:12 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hero-standing":return img`
+. . . . . 8 8 8 8 8 8 . . . . . 
+. . . . 8 6 6 6 6 6 6 8 . . . . 
+. . . 8 6 6 6 7 7 7 7 6 8 . . . 
+. . 8 6 7 6 8 6 6 6 8 7 6 8 . . 
+. . 8 7 7 7 6 8 8 8 8 8 7 8 . . 
+. . 8 7 7 b f d d f b 7 7 8 . . 
+. . 8 7 6 1 f d d f 1 6 7 8 . . 
+. . f 8 6 4 d d d d 4 6 8 f . . 
+. . f f f e 4 4 4 4 e f f f . . 
+. . . f c 2 e e e e b c f . . . 
+. . f d d c 2 b b b c d d f . . 
+. . f d d c b 2 2 b c d d f . . 
+. . . f f e e 5 5 e e f f . . . 
+. . . . f 4 4 4 4 4 4 f . . . . 
+. . . . f a c f f c a f . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,63 @@
+namespace SpriteKind {
+    export const StatusBar = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    showMinimap = !(showMinimap)
+    if (showMinimap) {
+        myMinimap.setFlag(SpriteFlag.Invisible, false)
+    } else {
+        myMinimap.setFlag(SpriteFlag.Invisible, true)
+    }
+})
+function generateMap () {
+    clearMap()
+    for (let index = 0; index < 3; index++) {
+        localHeight = mapSplit(randint(20, 80))
+        localWidth = mapSplit(randint(20, 80))
+        localX = mapSplit(randint(0, 40))
+        localY = mapSplit(randint(0, 40))
+        mapGen.generateTerrain(
+        [
+        assets.tile`transparency8`,
+        assets.tile`tile-sand`,
+        assets.tile`tile-sand`,
+        assets.tile`tile-grass`,
+        assets.tile`tile-grass`,
+        assets.tile`tile-mountain`,
+        assets.tile`tile-mountain`,
+        assets.tile`tile-mountain`
+        ],
+        mapSplit(16),
+        true,
+        localY,
+        localY + localHeight,
+        localX,
+        localX + localWidth
+        )
+    }
+}
+function mapSplit (percent: number) {
+    return percent / 100 * tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.Columns)
+}
+function clearMap () {
+    for (let localX = 0; localX <= tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.Columns); localX++) {
+        for (let localY = 0; localY <= tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.Rows); localY++) {
+            tiles.setTileAt(tiles.getTileLocation(localX, localY), assets.tile`tile-deep-ocean`)
+        }
+    }
+}
+let localY = 0
+let localX = 0
+let localWidth = 0
+let localHeight = 0
+let showMinimap = false
+let myMinimap: Sprite = null
+tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`tilemap-tiny-island`))
+generateMap()
+let hero = sprites.create(assets.image`hero-standing`, SpriteKind.Player)
+controller.moveSprite(hero, 96, 96)
+scene.cameraFollowSprite(hero)
+myMinimap = sprites.create(minimap.getImage(minimap.minimap(MinimapScale.Eighth, 2, 15)), SpriteKind.Player)
+myMinimap.setStayInScreen(true)
+myMinimap.setFlag(SpriteFlag.Invisible, true)
+showMinimap = false

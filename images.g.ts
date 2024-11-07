0fb96af48b0a6a8f7ebc20a7d641e032884e3060
AlexSzeto <Alex.Szeto@gmail.com>
2024-11-06 20:12:25 -0800

    hero walk wip

--- images.g.ts
+++ images.g.ts
@@ -3,8 +3,8 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "hero-standing":return img`
+            case "image2":
+            case "hero-standing0":return img`
 . . . . . 8 8 8 8 8 8 . . . . . 
 . . . . 8 6 6 6 6 6 6 8 . . . . 
 . . . 8 6 6 6 7 7 7 7 6 8 . . . 
@@ -22,6 +22,25 @@ namespace myImages {
 . . . . f a c f f c a f . . . . 
 . . . . . f f . . f f . . . . . 
 `;
+            case "image1":
+            case "hero-standing":return img`
+. . . . f f f f f f f . . . . . 
+. . . f 6 7 7 7 7 7 7 f . . . . 
+. . f 6 7 6 7 7 7 7 7 7 f . . . 
+. . f 6 7 7 8 6 6 6 7 7 6 f . . 
+. . f 8 6 8 4 8 8 8 6 6 6 f . . 
+. . f 8 8 4 d f b d 8 6 6 f . . 
+. . f 8 f d d f 1 d 4 8 6 f . . 
+. . f 6 f d d d d 4 f 6 6 f . . 
+. . f f 6 f f 4 4 f f 6 6 f . . 
+. . . . f a 2 b f f b f f . . . 
+. . . . f 2 b f d d f f . . . . 
+. . . . f b b f d d f f . . . . 
+. . . . f 5 e e f f e f . . . . 
+. . . f 7 7 7 7 7 6 6 8 f . . . 
+. . . f f f d 4 e e f f f . . . 
+. . . . . . f f f f . . . . . . 
+`;
         }
         return null;
     })
@@ -31,71 +50,71 @@ namespace myImages {
             case "hero-walk-left":
             case "anim1":return [img`
 . . . . f f f f f f f . . . . . 
-. . . f 8 6 6 6 6 6 6 f . . . . 
-. . f 8 6 6 6 7 7 7 7 6 f . . . 
-. . f 8 6 6 6 8 7 7 7 7 6 f . . 
-. . f 8 6 8 8 8 8 7 7 7 7 f . . 
-. . f 8 8 d d f b 8 7 7 7 f . . 
-. . f 8 e d d f 1 d 8 7 7 f . . 
-. . . f e d d d d d 8 7 8 f . . 
-. . . . f e 4 4 4 8 8 f f . . . 
-. . . . f b e b c c e f . . . . 
-. . . . f c b c d d c f . . . . 
-. . . . f c b c d d c f . . . . 
-. . . . f 5 5 e c c e f . . . . 
-. . . . . f 4 4 4 4 4 f . . . . 
-. . . . . f c c c c f . . . . . 
-. . . . . . f f f f . . . . . . 
+. . . f 6 7 7 7 7 7 7 f . . . . 
+. . f 6 7 6 7 7 7 7 7 7 f . . . 
+. . f 6 7 7 8 6 6 6 7 7 6 f . . 
+. . f 8 6 8 4 8 8 8 6 6 6 f . . 
+. . f 8 8 4 d f b d 8 6 6 f . . 
+. . f 8 f d d f 1 d 4 f 6 f . . 
+. . f 6 f d d d d 4 f 6 6 f . . 
+. . f f 6 f f 4 4 f f 6 6 f . . 
+. . . . f f 2 c f f c f f . . . 
+. . . . f 2 b f d d f f . . . . 
+. . . . f b b f d d f f . . . . 
+. . . . f 5 e e f f c f . . . . 
+. . . f 9 7 7 7 7 6 6 8 f . . . 
+. . . f f f d 4 f e f f f . . . 
+. . . . . f f f f f . . . . . . 
 `, img`
 . . . . . . . . . . . . . . . . 
 . . . . f f f f f f f . . . . . 
-. . . f 8 6 6 6 6 6 6 f . . . . 
-. . f 8 6 6 6 7 7 7 7 6 f . . . 
-. . f 8 6 6 6 8 7 7 7 7 6 f . . 
-. . f 8 6 8 8 8 8 7 7 7 7 f . . 
-. . f 8 8 d d f b 8 7 7 7 f . . 
-. . f 8 e d d f 1 d 8 7 7 f . . 
-. . . f e d d d d d 8 7 8 f . . 
-. . . . f e 4 4 4 8 8 f f . . . 
-. . . f f b e e e e b c f . . . 
-. . . f f c b b b b c d d f . . 
-. . . . f c b b b b c d d f . . 
-. . . . f 5 5 e e e e f f . . . 
-. . . . f 4 4 f f 4 4 f . . . . 
+. . . f 6 7 7 7 7 7 7 f . . . . 
+. . f 6 7 6 7 7 7 7 7 7 f . . . 
+. . f 6 7 7 8 6 6 6 7 7 6 f . . 
+. . f 8 6 8 4 8 8 8 6 6 6 f . . 
+. . f 8 8 d d f b 4 8 6 6 f . . 
+. . f 8 e d d f 1 d 4 8 6 f . . 
+. . f 6 e d d d d d f 6 6 f . . 
+. . f f f f 4 4 4 f f 6 6 f . . 
+. . f d f b e b c c f f f f . . 
+. . f d f c b b b b f d d f . . 
+. . . f f c b b b b f d d f . . 
+. . . . f 5 5 e e e f f f . . . 
+. . . . f 4 4 f f 4 . . . . . . 
 . . . . f f f . . f f f . . . . 
 `, img`
 . . . . f f f f f f f . . . . . 
-. . . f 8 6 6 6 6 6 6 f . . . . 
-. . f 8 6 6 6 7 7 7 7 6 f . . . 
-. . f 8 6 6 6 8 7 7 7 7 6 f . . 
-. . f 8 6 8 8 8 8 7 7 7 7 f . . 
-. . f 8 8 d d f b 8 7 7 7 f . . 
-. . f 8 e d d f 1 d 8 7 7 f . . 
-. . . f e d d d d d 8 7 8 f . . 
-. . . . f e 4 4 4 8 8 f f . . . 
-. . . . f b e b c c e f . . . . 
-. . . . f c b c d d c f . . . . 
-. . . . f c b c d d c f . . . . 
-. . . . f 5 5 e c c e f . . . . 
-. . . . . f 4 4 4 4 4 f . . . . 
-. . . . . f c c c c f . . . . . 
-. . . . . . f f f f . . . . . . 
+. . . f 6 7 7 7 7 7 7 f . . . . 
+. . f 6 7 6 7 7 7 7 7 7 f . . . 
+. . f 6 7 7 8 6 6 6 7 7 6 f . . 
+. . f 8 6 8 4 8 8 8 6 6 6 f . . 
+. . f 8 8 4 d f b d 8 6 6 f . . 
+. . f 8 f d d f 1 d 4 f 6 f . . 
+. . f 6 f d d d d 4 f 6 6 f . . 
+. . f f 6 f f 4 4 f f 6 6 f . . 
+. . . . f f 2 f f b c f f . . . 
+. . . . f 2 f d d f b f . . . . 
+. . . . f b f d d f b f . . . . 
+. . . . f 5 e f f b c f . . . . 
+. . . f 9 7 7 7 7 6 6 8 f . . . 
+. . . f f f d 4 f e f f f . . . 
+. . . . . f f f f f . . . . . . 
 `, img`
 . . . . . . . . . . . . . . . . 
 . . . . f f f f f f f . . . . . 
-. . . f 8 6 6 6 6 6 6 f . . . . 
-. . f 8 6 6 6 7 7 7 7 6 f . . . 
-. . f 8 6 6 6 8 7 7 7 7 6 f . . 
-. . f 8 6 8 8 8 8 7 7 7 7 f . . 
-. . f 8 8 d d f b 8 7 7 7 f . . 
-. . f 8 e d d f 1 d 8 7 7 f . . 
-. . . f e d d d d d 8 7 8 f . . 
-. . . . f e 4 4 4 8 8 f f . . . 
-. . . . f c e e e e b c f . . . 
-. . . f d d c b b b b c f f . . 
-. . . f d d c b b b b c f f . . 
-. . . . f c 5 e e e e f f . . . 
-. . . . f 4 4 f . f 4 4 f . . . 
+. . . f 6 7 7 7 7 7 7 f . . . . 
+. . f 6 7 6 7 7 7 7 7 7 f . . . 
+. . f 6 7 7 8 6 6 6 7 7 6 f . . 
+. . f 8 6 8 4 8 8 8 6 6 6 f . . 
+. . f 8 8 d d f b 4 8 6 6 f . . 
+. . f 8 e d d f 1 d 4 8 6 f . . 
+. . f 6 e d d d d d f 6 6 f . . 
+. . f f 6 f 4 4 4 f f 6 6 f . . 
+. . . . f f f e c c e f f . . . 
+. . . . f d d f b b b c f f . . 
+. . . . f d d f b b b c f f . . 
+. . . . f f f 5 e e e f f . . . 
+. . . f f 4 4 f . f 4 4 f f . . 
 . . . . . f f . . . f f f . . . 
 `];
         }
--- main.ts
+++ main.ts
@@ -9,6 +9,28 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         myMinimap.setFlag(SpriteFlag.Invisible, true)
     }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    animHeroWalkLeft,
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, hero)
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, hero)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    animHeroWalkRight,
+    100,
+    true
+    )
+})
 function generateMap () {
     clearMap()
     for (let index = 0; index < 3; index++) {
@@ -52,9 +74,17 @@ let localWidth = 0
 let localHeight = 0
 let showMinimap = false
 let myMinimap: Sprite = null
+let hero: Sprite = null
+let animHeroWalkRight: Image[] = []
+let animHeroWalkLeft: Image[] = []
 tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`tilemap-tiny-island`))
 generateMap()
-let hero = sprites.create(assets.image`hero-standing`, SpriteKind.Player)
+animHeroWalkLeft = assets.animation`hero-walk-left`
+animHeroWalkRight = assets.animation`hero-walk-left`
+for (let localImage of animHeroWalkRight) {
+    localImage.flipX()
+}
+hero = sprites.create(assets.image`hero-standing`, SpriteKind.Player)
 controller.moveSprite(hero, 96, 96)
 scene.cameraFollowSprite(hero)
 myMinimap = sprites.create(minimap.getImage(minimap.minimap(MinimapScale.Eighth, 2, 15)), SpriteKind.Player)

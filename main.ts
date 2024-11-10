e118a52c473c30c13f43f294c12bc6347978906d
AlexSzeto <Alex.Szeto@gmail.com>
2024-11-10 00:35:46 -0800

    Restore '11/6/2024, 7:04:43 PM walk left anim'

--- main.ts
+++ main.ts
@@ -9,28 +9,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         myMinimap.setFlag(SpriteFlag.Invisible, true)
     }
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    hero,
-    animHeroWalkLeft,
-    100,
-    true
-    )
-})
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, hero)
-})
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, hero)
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    hero,
-    animHeroWalkRight,
-    100,
-    true
-    )
-})
 function generateMap () {
     clearMap()
     for (let index = 0; index < 3; index++) {
@@ -74,17 +52,9 @@ let localWidth = 0
 let localHeight = 0
 let showMinimap = false
 let myMinimap: Sprite = null
-let hero: Sprite = null
-let animHeroWalkRight: Image[] = []
-let animHeroWalkLeft: Image[] = []
 tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`tilemap-tiny-island`))
 generateMap()
-animHeroWalkLeft = assets.animation`hero-walk-left`
-animHeroWalkRight = assets.animation`hero-walk-left`
-for (let localImage of animHeroWalkRight) {
-    localImage.flipX()
-}
-hero = sprites.create(assets.image`hero-standing`, SpriteKind.Player)
+let hero = sprites.create(assets.image`hero-standing`, SpriteKind.Player)
 controller.moveSprite(hero, 96, 96)
 scene.cameraFollowSprite(hero)
 myMinimap = sprites.create(minimap.getImage(minimap.minimap(MinimapScale.Eighth, 2, 15)), SpriteKind.Player)

684e8b813f63651be3d59c7dc5dca66f309f5c5f
Alex Szeto <Alex.Szeto@gmail.com>
2024-11-13 20:59:30 -0800

    single frame use animations

---
+++ backup.ts
@@ -0,0 +1,59 @@
+/*
+// use-up wind up frame
+img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 7 7 7 7 7 7 6 f . . . 
+. . . f 7 7 7 7 7 7 6 6 6 f . . 
+. . f 7 7 6 6 6 6 6 6 6 6 f . . 
+. . f 7 7 6 6 6 6 6 6 6 6 f . . 
+. . f 7 7 7 6 6 6 6 6 6 8 f . . 
+. . f 7 7 7 6 6 6 6 6 8 8 f . . 
+. . f 7 7 7 6 6 6 6 8 8 f f . . 
+. . f f f f f f f f f f f d f . 
+. . . . f d d f b c c f d d f . 
+. . . . f d d f 7 6 6 f f f . . 
+. . . . . f f f f e e f . . . . 
+. . . . . . . . . f f . . . . . 
+`
+// use-down wind up frame
+img`
+. . . . f f f f f f f . . . . . 
+. . . f 7 7 7 7 7 7 7 f . . . . 
+. . f 6 7 7 7 7 7 6 7 7 f . . . 
+. . f 7 6 7 7 7 7 7 8 7 6 f . . 
+. . f 7 8 8 6 6 8 8 8 6 6 f . . 
+. . f 7 f b f d d f b f 6 f . . 
+. . f f 4 1 f d d f 1 4 f f . . 
+. . f 7 f 4 d d d d 4 f 6 f . . 
+. . f f 7 f 4 4 4 4 f f f f . . 
+. f d d f f c c c f f f . . . . 
+. f d d f f b b b f d f . . . . 
+. . f f . f b b b f f . . . . . 
+. . . . . f 7 7 6 f . . . . . . 
+. . . . . f e e f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+// use-left wind up frame
+img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f f . . . . . . . 
+. f 6 7 7 7 7 7 7 f . . . . . . 
+f 6 7 6 7 7 7 7 7 7 f . . . . . 
+f 6 7 7 8 6 6 6 7 7 7 f . . . . 
+f 6 6 8 4 8 8 8 6 7 7 f . . . . 
+f 6 8 4 d f b d 8 7 7 f . . . . 
+f 6 f d d f 1 d 4 f 7 f . . . . 
+f 6 f d d d d 4 f 7 7 f . . . . 
+. f 6 f f 4 4 f 7 7 7 f . . . . 
+. f f f c c f f f f f f . . . . 
+f d d f c c c f f . . . . . . . 
+f d d f f c c c f . . . . . . . 
+f f f 7 7 6 6 6 6 f f . . . . . 
+. . . f e e f f f e e f . . . . 
+. . . f f f . . f f f f . . . . 
+`
+*/
